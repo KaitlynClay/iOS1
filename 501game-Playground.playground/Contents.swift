2b241e12f4355add8c9de1dfc39c29f771dcf55f
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2023-08-28 14:39:29 -0500

    Add files via upload

---
+++ 501game-Playground.playground/Contents.swift
@@ -0,0 +1,133 @@
+var robPoints = 501
+var jayPoints = 501
+var benPoints = 501
+// you == Jay
+var roundRob = 0
+var roundJay = 0
+var roundBen = 0
+
+
+roundRob += 4
+roundRob += 25
+roundRob += 10
+robPoints -= roundRob
+roundRob = 0
+
+roundJay += 17
+roundJay += 50
+roundJay += 3
+jayPoints -= roundJay
+roundJay = 0
+
+roundBen += 36
+roundBen += 6
+roundBen += 6
+benPoints -= roundBen
+roundBen = 0
+
+print("Jay? Thought you said you were bad at this?")
+print("Must be beginners luck, Ben")
+
+roundRob += 16
+roundRob += 48
+roundRob += 50
+robPoints -= roundRob
+roundRob = 0
+
+roundJay += 13
+roundJay += 25
+roundJay += 16
+jayPoints -= roundJay
+roundJay = 0
+
+roundBen += 19
+roundBen += 16
+roundBen += 38
+benPoints -= roundBen
+roundBen = 0
+
+print("Must have been Jay.")
+
+roundRob += 8
+roundRob += 26
+roundRob += 19
+robPoints -= roundRob
+roundRob = 0
+
+roundJay += 20
+roundJay += 19
+roundJay += 36
+jayPoints -= roundJay
+roundJay = 0
+
+roundBen += 38
+roundBen += 30
+roundBen += 20
+benPoints -= roundBen
+roundBen = 0
+
+print("Interested to see which of us wins, Ben")
+print("That it will be, Rob")
+
+roundRob += 16
+roundRob += 40
+roundRob += 30
+robPoints -= roundRob
+roundRob = 0
+
+roundJay += 25
+roundJay += 28
+roundJay += 50
+jayPoints -= roundJay
+roundJay = 0
+
+roundBen += 12
+roundBen += 20
+roundBen += 18
+benPoints -= roundBen
+roundBen = 0
+
+print("Nice round")
+
+roundRob += 18
+roundRob += 28
+roundRob += 50
+robPoints -= roundRob
+roundRob = 0
+
+roundJay += 36
+roundJay += 28
+roundJay += 36
+jayPoints -= roundJay
+roundJay = 0
+
+roundBen += 19
+roundBen += 25
+roundBen += 50
+benPoints -= roundBen
+roundBen = 0
+
+print("Looks like the game will be a pretty qick game!")
+
+roundRob += 50
+roundRob += 20
+roundRob += 25
+robPoints -= roundRob
+roundRob = 0
+
+roundJay += 25
+roundJay += 25
+roundJay += 50
+jayPoints -= roundJay
+roundJay = 0
+
+roundBen += 30
+roundBen += 25
+roundBen += 50
+benPoints -= roundBen
+roundBen = 0
+
+print("You won Jay?")
+print("Looks that way, Rob")
+print("We'll have to make sure we have a rematch at some point.")
+print("Agreed")
