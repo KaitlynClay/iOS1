77b952b0e83d7a00213c73e626daa93c40d09bdc
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2023-09-06 14:27:10 -0500

    Add files via upload

---
+++ criticalThinkingOperators.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//comparing prices
+
+var a = 10
+var b = 25
+var j = 3
+var k = 9
+var x = 12
+var y = 32
+
+if a == j {
+    print("These products are the same price!")
+} else {
+    print("These products are not the same price.")
+}
+
+if b <= y {
+    print("The first product is cheaper!")
+} else {
+    print("The second product is cheaper than the first product.")
+}
+
+if k >= x {
+    print("The first product is more expensive than the second product.")
+} else {
+    print("The second product is more expensive.")
+}
